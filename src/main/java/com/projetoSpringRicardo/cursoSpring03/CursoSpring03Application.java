640d678c9eec894322c63f26ce0864a3899a3762
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-12-06 19:24:20 -0300

    Criacao do projeto

---
+++ src/main/java/com/projetoSpringRicardo/cursoSpring03/CursoSpring03Application.java
@@ -0,0 +1,13 @@
+package com.projetoSpringRicardo.cursoSpring03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpring03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpring03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoSpringRicardo/cursoSpring03/CursoSpring03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoSpringRicardo.cursoSpring03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpring03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
