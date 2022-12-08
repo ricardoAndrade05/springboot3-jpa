328ac13b1bc4a9277014b7ec53881a3ca22973f7
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-12-08 15:49:53 -0300

    Enum OrderStatus

--- src/main/java/com/projetoSpringRicardo/cursoSpring03/config/TesteConfig.java
+++ src/main/java/com/projetoSpringRicardo/cursoSpring03/config/TesteConfig.java
@@ -1,7 +1,6 @@
 package com.projetoSpringRicardo.cursoSpring03.config;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.projetoSpringRicardo.cursoSpring03.entities.Order;
 import com.projetoSpringRicardo.cursoSpring03.entities.User;
+import com.projetoSpringRicardo.cursoSpring03.entities.enums.OrderStatus;
 import com.projetoSpringRicardo.cursoSpring03.repositories.OrderRepository;
 import com.projetoSpringRicardo.cursoSpring03.repositories.UserRepository;
 
@@ -33,9 +33,9 @@ public class TesteConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(usuarios);
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
 		List<Order> pedidos = Arrays.asList(o1,o2,o3);
 		
