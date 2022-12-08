e7bfcc018b0d28512b5eae0fb99cad902fa5f996
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-12-08 17:33:48 -0300

    Product + Associacao many-to-many com JoinTable

--- src/main/java/com/projetoSpringRicardo/cursoSpring03/config/TesteConfig.java
+++ src/main/java/com/projetoSpringRicardo/cursoSpring03/config/TesteConfig.java
@@ -55,19 +55,27 @@ public class TesteConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
-		List<Category> categorias = Arrays.asList(cat1,cat2,cat3);
-		
-		categoryRepository.saveAll(categorias);
-		
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
-		List<Product> produtos = Arrays.asList(p1,p2,p3,p4,p5);
-		
+		p1.getCategories().add(cat2);
+		p2.getCategories().addAll(Arrays.asList(cat1,cat3));
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+	
+        List<Category> categorias = Arrays.asList(cat1,cat2,cat3);
+	    List<Product> produtos = Arrays.asList(p1,p2,p3,p4,p5);
+	
+		categoryRepository.saveAll(categorias);
 		productRepository.saveAll(produtos);
+		
+
+		
+		
 		
 	}
 	
