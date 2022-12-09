c4de38f009a2716fa367365b9069c7b39e0ffb3b
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-12-08 22:50:49 -0300

    Payment, associacao one-to-one

--- src/main/java/com/projetoSpringRicardo/cursoSpring03/config/TesteConfig.java
+++ src/main/java/com/projetoSpringRicardo/cursoSpring03/config/TesteConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetoSpringRicardo.cursoSpring03.entities.Category;
 import com.projetoSpringRicardo.cursoSpring03.entities.Order;
 import com.projetoSpringRicardo.cursoSpring03.entities.OrderItem;
+import com.projetoSpringRicardo.cursoSpring03.entities.Payment;
 import com.projetoSpringRicardo.cursoSpring03.entities.Product;
 import com.projetoSpringRicardo.cursoSpring03.entities.User;
 import com.projetoSpringRicardo.cursoSpring03.entities.enums.OrderStatus;
@@ -51,6 +52,9 @@ public class TesteConfig implements CommandLineRunner {
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
+		
+		Payment pagamento = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pagamento);
 		
 		List<Order> pedidos = Arrays.asList(o1,o2,o3);
 		
