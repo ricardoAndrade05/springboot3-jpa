7a833612b2cb4d157a9de7c2d0b4ecdb61a8e172
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-12-08 23:44:25 -0300

    User update

--- src/main/java/com/projetoSpringRicardo/cursoSpring03/services/UserService.java
+++ src/main/java/com/projetoSpringRicardo/cursoSpring03/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetoSpringRicardo.cursoSpring03.entities.User;
@@ -11,25 +12,37 @@ import com.projetoSpringRicardo.cursoSpring03.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User user) {
 		return repository.save(user);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity,obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 
 }
